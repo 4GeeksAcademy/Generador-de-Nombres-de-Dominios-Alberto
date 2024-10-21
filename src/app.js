02250250e22558112d4746f3c76db2f4d95540d8
SrAlbert0 <73313619+SrAlbert0@users.noreply.github.com>
2024-10-21 18:26:17 +0000

    Exercice resolution with error

--- src/app.js
+++ src/app.js
@@ -8,4 +8,16 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  pronoun.forEach(pron => {
+    adj.forEach(adj => {
+      noun.forEach(noun => {
+        console.log(pron + adj + noun + ".com");
+      });
+    });
+  });
 };
